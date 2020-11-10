9f4011ec777dffc894193a260b7bc843d8efce33
Jarod <jarodsim@gmail.com>
2020-11-10 14:15:54 -0300

    styles: adicionando frase no footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -10,35 +10,38 @@ export default function Footer() {
 
     return (
         <div className={styles.container}>
-            <div className={styles.links}>
-                <h3>Acesso Rápido</h3>
-                <ul>
-                    <li><Link href="#planos">Planos</Link></li>
-                    <li><Link href="#onde">Onde Atendemos</Link></li>
-                    <li><Link href="#quem_somos">Quem Somos</Link></li>
-                    <li><Link href="#topo">Topo</Link></li>
-                </ul>
-            </div>
+            <h3 className={styles.frase}>"Mais que produtos, vendemos soluções!"</h3>
+            <div className={styles.info}>
+                <div className={styles.links}>
+                    <h3>Acesso Rápido</h3>
+                    <ul>
+                        <li><Link href="#planos">Planos</Link></li>
+                        <li><Link href="#onde">Onde Atendemos</Link></li>
+                        <li><Link href="#quem_somos">Quem Somos</Link></li>
+                        <li><Link href="#topo">Topo</Link></li>
+                    </ul>
+                </div>
 
-            <div className={styles.endereco}>
-                <h3>Onde Nos Encontrar</h3>
-                <address>
-                    Francisco Ayres - PI, <br />
-                    Rua Tito Ferreia, 420 - Bairro Centro
-                </address>
-            </div>
+                <div className={styles.endereco}>
+                    <h3>Onde Nos Encontrar</h3>
+                    <address>
+                        Francisco Ayres - PI, <br />
+                Rua Tito Ferreia, 420 - Bairro Centro
+            </address>
+                </div>
 
-            <div className={styles.contato}>
-                <h3>Formas de Contato</h3>
-                <ul>
-                    <li onClick={() => sendWhatsappMessage()}>
-                        Nos mande uma mensagem pelo whatsapp: 89994578337
-                    </li>
-                    <li>
-                        Ou nos ligue em nosso 0800, é de graça :) 08000420497
-                    </li>
-                </ul>
+                <div className={styles.contato}>
+                    <h3>Formas de Contato</h3>
+                    <ul>
+                        <li onClick={() => sendWhatsappMessage()}>
+                            Nos mande uma mensagem pelo whatsapp: 89994578337
+                </li>
+                        <li>
+                            Ou nos ligue em nosso 0800, é de graça :) 08000420497
+                </li>
+                    </ul>
+                </div>
             </div>
-        </div>
+            <small>&copy; Todos os direitos reservados a info-tec - 2020</small></div>
     )
 }
